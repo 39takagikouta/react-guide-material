85e98c7b34656f0f179611df586c6fc4f44030ad
39takagikouta <kouta.takagi0728@gmail.com>
2024-04-28 20:19:29 +0900

    4終了

--- 04_react_basic/src/070_component_nest/start/Example.js
+++ 04_react_basic/src/070_component_nest/start/Example.js
@@ -8,8 +8,8 @@ const Example = () => {
         <li>item-1</li>
         <li>item-2</li>
         <li>item-3</li>
-        <li>item-3</li>
-        <li>item-3</li>
+        <li>item-4</li>
+        <li>item-5</li>
       </ul>
     </div>
   );
--- 04_react_basic/src/100_practice_props/start/components/Profile.js
+++ 04_react_basic/src/100_practice_props/start/components/Profile.js
@@ -1,12 +1,13 @@
 import "./Profile.css";
 
-const Profile = (props) => {
+const Profile = ({ name = "John Doe", age, country }) => {
   return (
     <div className="profile">
-
+      <h3>{"name:" + name}</h3>
+      <p>{"age:" + age}</p>
+      <p>{"name:" + country}</p>
     </div>
   );
 };
-
 
 export default Profile;
