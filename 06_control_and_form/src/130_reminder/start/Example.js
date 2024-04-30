dec336a0e5ad7826159e458c27769a7c236fb2f5
39takagikouta <kouta.takagi0728@gmail.com>
2024-04-30 16:14:29 +0900

    6終了

--- 06_control_and_form/src/130_reminder/start/Example.js
+++ 06_control_and_form/src/130_reminder/start/Example.js
@@ -1,8 +1,45 @@
+import { useState } from "react";
+
 const Example = () => {
+  const todosList = [
+    {
+      id: 1,
+      content: "店予約する",
+    },
+    {
+      id: 2,
+      content: "卵買う",
+    },
+    {
+      id: 3,
+      content: "郵便出す",
+    },
+  ];
+  const [todos, setTodos] = useState(todosList);
+
+  const completeTodo = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  const addTodo = (todo) => {
+    console.log(todo);
+  };
+
   return (
     <>
-      <h3>完成系と同じような機能を作成してください。</h3>
-      <p>また、枠線で囲まれた部品をコンポーネントに分離してください。</p>
+      {todos.map((todo) => {
+        return (
+          <div key={todo.id}>
+            <button onClick={() => completeTodo(todo.id)}>完了</button>
+            <span>{todo.content}</span>
+          </div>
+        );
+      })}
+      <form onSubmit={(e) => addTodo(e.target.value)}>
+        <input type="text" />
+        <button>追加</button>
+      </form>
     </>
   );
 };
