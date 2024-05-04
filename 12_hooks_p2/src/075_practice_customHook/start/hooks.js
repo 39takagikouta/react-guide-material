52d78798dacc44410543496b2bea38e05781f9ef
39takagikouta <kouta.takagi0728@gmail.com>
2024-05-04 15:41:35 +0900

    11完了

---
+++ 12_hooks_p2/src/075_practice_customHook/start/hooks.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export const useCount = () => {
+  const [count, setCount] = useState();
+  const countUp = () => {
+    setCount(count + 1);
+  };
+  return {
+    count,
+    countUp,
+  };
+};
